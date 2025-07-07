00123f49766f3da4fbe842e619a792116c572fa7
Codewithshaik <imoxygod@gmail.com>
2025-07-08 00:25:53 +0530

    changes includes OTP and gmail login scripts

--- src/test/java/com/shaiksnet/stepdefintions/AccountDetailsPageStep.java
+++ src/test/java/com/shaiksnet/stepdefintions/AccountDetailsPageStep.java
@@ -1,10 +1,60 @@
 package com.shaiksnet.stepdefintions;
 
 import com.shaiksnet.pages.AccountDetailsPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.datatable.DataTable;
 
 public class AccountDetailsPageStep {
     private final AccountDetailsPage accountDetailsPage = new AccountDetailsPage(Hooks.getDriver());
 
 
+    @When("the user update naukri keywords")
+    public void theUserUpdateNaukriKeywords(DataTable dataTable) {
+        AccountDetailsPage.theUserUpdateNaukriKeywords(dataTable);
+    }
 
+    @When("the user updates naukri resume")
+    public void theUserUpdatesNaukriResume(DataTable dataTable) {
+
+    }
+
+    @When("the user updates naukri profile")
+    public void theUserUpdatesNaukriProfile(DataTable dataTable) {
+
+    }
+
+    @When("the user updates naukri job preferences")
+    public void theUserUpdatesNaukriJobPreferences() {
+
+    }
+
+    @When("the user updates naukri job alerts")
+    public void theUserUpdatesNaukriJobAlerts() {
+
+    }
+
+    @When("the user updates naukri job applications")
+    public void theUserUpdatesNaukriJobApplications() {
+
+    }
+
+    @When("the user updates naukri job search")
+    public void theUserUpdatesNaukriJobSearch(DataTable dataTable) {
+
+    }
+
+    @Then("the user should see the updated naukri profile")
+    public void theUserShouldSeeTheUpdatedNaukriProfile() {
+    }
+
+    @And("user update the resume and download")
+    public void userUpdateTheResumeAndDownload() {
+        
+    }
+
+    @Then("user uploads the resume to naukri profile")
+    public void userUploadsTheResumeToNaukriProfile() {
+    }
 }
--- src/test/resources/features/ApplyMate_Naukri.feature
+++ src/test/resources/features/ApplyMate_Naukri.feature
@@ -1,10 +1,29 @@
 @Naukri
-  Feature: Naukri Apply mate
-    I want to use this template for naukri daily login and resume
+Feature: Naukri Apply mate
+  I want to use this template for naukri daily login and resume
 
   Background:
     Given the user logs into naukri.com
+      | shaikmahaboobsubhani0777@gmail.com |
+      | WXVjYW50Y2F0Y2htZTEyISE=           |
 
 
-    Scenario: logging into the net
-      When the user logs into US Check visa slots with valid credentials
+  Scenario: updating naukri profile with keywords, job application and resume
+    When the user update naukri keywords
+      | keywords |
+      | java     |
+      | selenium |
+      | rest api |
+
+    When the user updates naukri job search
+      | jobSearch     |
+      | software jobs |
+
+    And the user login into flowCv
+      | imoxygod@gmail.com |
+      | $password          |
+
+    And user update the resume and download
+    Then user uploads the resume to naukri profile
+    Then the user should see the updated naukri profile
+
